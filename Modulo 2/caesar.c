88b96fbac95c8f7c9698ca2c95ae8067311a6fb3
Gustavo Monnerat <gugammcv@gmail.com>
2022-07-16 19:16:57 -0300

    tests

--- Modulo 2/caesar.c
+++ Modulo 2/caesar.c
@@ -1,23 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 /*
 INCOMPLETE
 */
 
-
 int main(int argc, char *argv[]){
+
+	int status = 0;
+
+	for(int i = 0; argv[i] != '\0'; i++)
+	{
+		if (status==1){
+            break;
+        }
+		while(isdigit(argv[i])){
+			printf("%c", argv[i]);
+			
+			if(isdigit(argv[i + 1])){
+				status = 1;
+			}
+			i++;
+		}
+	}
+
+    printf("%d", status);
 
     if(argc > 2 || argc < 2){
         printf("Usage: .\\caesar key");
+
+    }else if(status == 1){
+        printf("Usage: .\\caesar key");
+
     }else{
         char text[300] = {'\0'};
         int key = 0;
 
         key = atoi(argv[1]);
-    
-        printf("\n\n%d\n\n", argc);
 
         printf("Text: ");
         scanf(" %[^\n]", text);
@@ -29,7 +50,7 @@ int main(int argc, char *argv[]){
         }
 
         printf("%s", text);
+    }
 
-        }
     return 0;
 }
